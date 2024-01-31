166961d17df22db186157787d9020d8bb4129890
ethan-ung <ethan.s.ung04pv@gmail.com>
2024-01-31 16:38:18 -0600

    1:54:54 in

--- network.js
+++ network.js
@@ -1,3 +1,24 @@
+class NeuralNetwork{
+    constructor(neuronCounts){
+        this.levels=[];
+        for(let i=0;i<neuronCounts.length-1;i++){
+            this.levels.push(new Level(
+                neuronCounts[i],neuronCounts[i+1]
+            ));
+        }
+    }
+
+    static feedForward(givenInputs,network){
+        let outputs=Level.feedForward(
+            givenInputs,network.levels[0]);
+        for(let i=1;i<network.levels.length;i++){
+            outputs=Level.feedForward(
+                outputs,network.levels[i]);
+        }
+        return outputs;
+    }
+}
+
 class Level{
     constructor(inputCount,outputCount){
         this.inputs=new Array(inputCount);
@@ -24,6 +45,22 @@ class Level{
     }
 
     static feedForward(givenInputs,level){
-        
+        for(let i=0;i<level.inputs.length;i++){
+            level.inputs[i]=givenInputs[i];
+        }
+
+        for(let i=0;i<level.outputs.length;i++){
+            let sum=0;
+            for(let j=0;j<level.inputs.length;j++){
+                sum+=level.inputs[j]*level.weights[j][i];
+            }
+
+            if(sum>level.biases[i]){
+                level.outputs[i]=1;
+            }else{
+                level.outputs[i]=0;
+            }
+        }
+        return level.outputs;
     }
 }
