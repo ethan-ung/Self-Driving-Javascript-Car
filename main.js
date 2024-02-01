9702cc740d8037d9f7480be4392557b7c6bed36d
ethan-ung <ethan.s.ung04pv@gmail.com>
2024-02-01 15:44:05 -0600

    Finished

--- main.js
+++ main.js
@@ -9,9 +9,34 @@ const road=new Road(carCanvas.width/2,carCanvas.width*0.9);
 
 const N=100;
 const cars = generateCars(N);
+let bestCar=cars[0];
+if(localStorage.getItem("bestBrain")){
+    for(let i=0;i<cars.length;i++){
+        cars[i].brain=JSON.parse(
+            localStorage.getItem("bestBrain"));
+        if(i!=0){
+            NeuralNetwork.mutate(cars[i].brain,0.15);
+        }
+    }
+}
 
 const traffic=[
-    new Car(road.getLaneCenter(1),-100,30,50,"DUMMY",2)
+    new Car(road.getLaneCenter(1),-100,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(0),-300,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(2),-300,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(1),-500,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(0),-500,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(0),-700,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(2),-900,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(0),-1100,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(1),-1100,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(0),-1300,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(2),-1300,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(2),-1500,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(1),-1500,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(0),-1700,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(1),-1700,30,50,"DUMMY",2),
+    
 ];
 animate();
 
@@ -21,7 +46,7 @@ function save(){
 }
 
 function discard(){
-    localStorage
+    localStorage.removeItem("bestBrain");
 }
 
 function generateCars(N){
@@ -39,7 +64,7 @@ function animate(time){
     for(let i=0;i<cars.length;i++){
         cars[i].update(road.borders,traffic);
     }
-    const bestCar=cars.find(
+    bestCar=cars.find(
         c=>c.y==Math.min(
             ...cars.map(c=>c.y)
         ));
